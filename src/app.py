cd0ab34e0a270e91de229e76d5385771068eba1e
Sarun6618 <ch.sarun0904@gmail.com>
2023-11-19 16:37:57 +0530

    updated signup and remove comments

--- src/app.py
+++ src/app.py
@@ -4,7 +4,6 @@ import jwt
 import os
 from dotenv import load_dotenv
 from flask import Flask, render_template, request, jsonify
-# from bson.objectid import ObjectId
 from flask_cors import CORS
 from flask_cors import cross_origin
 from validate import validate_email_and_password, validate_user
@@ -21,7 +20,6 @@ CORS(app, resources={r"/*": {"origins": ["http://localhost:4004", "http://192.16
 # CORS(app) 
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
-# print(SECRET_KEY)
 app.config["JWT_SECRET_KEY"] = os.environ.get('JWT_SECRET_KEY')
 app.config['SECRET_KEY'] = SECRET_KEY
 
@@ -30,40 +28,14 @@ app.config['SECRET_KEY'] = SECRET_KEY
 def hello():
     return render_template("home.html")
 
-# Render the login form
-
-
-# @app.route("/login", methods=["GET"])
-# def login_form():
-#     return render_template("login.html")
-
-# Render the signup form
-
-
 @app.route("/signup", methods=["GET"])
 def signup_form():
     return render_template("signup.html")
 
-# Render the dashboard with classes (replace with your logic)
-
-
-# @app.route("/dashboard", methods=["GET"])
-# def dashboard():
-#     # Replace with logic to fetch user data and classes
-#     user_data = {
-#         "name": "John Doe",
-#         "email": "john@example.com",
-#         "role": "teacher"
-#     }
-#     classes = ["Class A", "Class B", "Class C"]
-#     return render_template("dashboard.html", current_user=user_data, classes=classes)
 @app.route("/dashboard", methods=["GET"])
 def dashboard():
     try:
-        # Retrieve the token from the query parameters
         token = request.args.get('token')
-
-        # Verify the token
         try:
             decoded_token = jwt.decode(token, app.config["JWT_SECRET_KEY"], algorithms=["HS256"])
         except jwt.ExpiredSignatureError:
@@ -76,13 +48,9 @@ def dashboard():
                 "message": "Invalid token",
                 "error": "Unauthorized"
             }), 401
-
-        # Fetch user data based on the decoded token
         user = User().get_user_by_id(decoded_token["id"])
 
         if user:
-            # Fetch and sort classes by creation date (latest first)
-            # classes = Classes().get_all_classes_sorted_by_date()
             classes = list(db.classes.find({}))
             return render_template("dashboard.html", classes=classes, current_user=user)
 
@@ -109,12 +77,6 @@ def logout():
 def add_user():
     try:
         print("adduser")
-        # if current_user is None:
-        #     return {
-        #         "message": "Something went wrong && current_user is none",
-        #         "error": str(e),
-        #         "data": None
-        #     }, 500
         user = request.json
         if not user:
             return {
@@ -145,74 +107,9 @@ def add_user():
             "error": str(e),
             "data": None
         }, 500
-
-
-# @app.route("/login", methods=["POST"])
-# def login():
-#     try:
-#         data = request.json
-#         if not data:
-#             return {
-#                 "message": "Please provide user details",
-#                 "data": None,
-#                 "error": "Bad request"
-#             }, 400
-#         # validate input
-#         is_validated = validate_email_and_password(
-#             data.get('email'), data.get('password'))
-#         if is_validated is not True:
-#             return dict(message='Invalid data', data=None, error=is_validated), 400
-#         user = User().login(
-#             data["email"],
-#             data["password"]
-#         )
-#         if user:
-#             try:
-#                 # print(user["_id"])
-#                 payload = {
-#                     'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=6),
-#                     'id': user["_id"],
-#                     'name': user["name"],
-#                     'role': user["role"],
-#                     'section': user.get("section", ""),
-#                     'branch': user.get("branch", ""),
-#                 }
-#                 access_token = jwt.encode(
-#                     payload,
-#                     app.config["JWT_SECRET_KEY"],
-#                     algorithm="HS256"
-#                 )
-#                 return {"access_token": access_token, 'id': user["_id"],
-#                         'name': user["name"],
-#                         'role': user["role"],
-#                         'section': user["section"],
-#                         'branch': user["branch"]}, 200
-
-#             except Exception as e:
-#                 return {
-#                     "error": "Something went wrong",
-#                     "message": str(e)
-#                 }, 500
-#         return {
-#             "message": "Error fetching auth token!, invalid email or password",
-#             "data": None,
-#             "error": "Unauthorized"
-#         }, 404
-#     except Exception as e:
-#         return {
-#             "message": "Something went wrong!",
-#             "error": str(e),
-#             "data": None
-#         }, 500
-# Render the login form
 @app.route("/login", methods=["GET"])
 def login_form():
     return render_template("login.html")
-
-# Handle login form submission
-# Import the required modules
-
-# ...
 
 @app.route("/login", methods=["POST"])
 def login():
@@ -284,144 +181,6 @@ def login():
             "data": None
         }), 500
 
-
-# @app.route("/login", methods=["POST"])
-# @cross_origin(supports_credentials=True)
-# def login():
-#     try:
-#         data = request.json
-#         if not data:
-#             return jsonify({
-#                 "message": "Please provide user details",
-#                 "data": None,
-#                 "error": "Bad request"
-#             }), 400
-
-#         # validate input
-#         is_validated = validate_email_and_password(
-#             data.get('email'), data.get('password'))
-
-#         if is_validated is not True:
-#             return jsonify({
-#                 "message": "Invalid email or password",
-#                 "data": None,
-#                 "error": "Unauthorized"
-#             }), 401
-
-#         user = User().login(data["email"], data["password"])
-#         if user:
-#             try:
-#                 # Generate the JWT token
-#                 payload = {
-#                     'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=6),
-#                     'id': user["_id"],
-#                     'name': user["name"],
-#                     'role': user["role"],
-#                     'section': user.get("section", ""),
-#                     'branch': user.get("branch", ""),
-#                 }
-#                 access_token = jwt.encode(
-#                     payload,
-#                     app.config["JWT_SECRET_KEY"],
-#                     algorithm="HS256"
-#                 )
-
-#                 # Set the token as a cookie in the response
-#                 response = jsonify({
-#                     "access_token": access_token,
-#                     'id': user["_id"],
-#                     'name': user["name"],
-#                     'role': user["role"],
-#                     'section': user["section"],
-#                     'branch': user["branch"]
-#                 })
-                
-#                 # Set the access token as a cookie with HttpOnly and Secure flags
-#                 response.set_cookie('access_token', access_token, httponly=True, secure=True)
-                
-#                 return response, 200
-
-#             except Exception as e:
-#                 return jsonify({
-#                     "message": "Something went wrong",
-#                     "error": str(e),
-#                     "data": None
-#                 }), 500
-#         else:
-#             return jsonify({
-#                 "message": "Invalid email or password",
-#                 "data": None,
-#                 "error": "Unauthorized"
-#             }), 401
-
-#     except Exception as e:
-#         return jsonify({
-#             "message": "Something went wrong",
-#             "error": str(e),
-#             "data": None
-#         }), 500
-# def login():
-#     try:
-#         data = request.json
-#         if not data:
-#             return jsonify({
-#                 "message": "Please provide user details",
-#                 "data": None,
-#                 "error": "Bad request"
-#             }), 400
-
-#         # Validate input (you can keep this part)
-#         is_validated = validate_email_and_password(
-#             data.get('email'), data.get('password'))
-
-#         if is_validated is not True:
-#             return jsonify({
-#                 "message": "Invalid email or password",
-#                 "data": None,
-#                 "error": "Unauthorized"
-#             }), 401
-
-#         user = User().login(data["email"], data["password"])
-#         if user:
-#             try:
-#                 # Generate the JWT token
-#                 payload = {
-#                     'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=6),
-#                     'id': user["_id"],
-#                     'name': user["name"],
-#                     'role': user["role"],
-#                     'section': user.get("section", ""),
-#                     'branch': user.get("branch", ""),
-#                 }
-#                 access_token = jwt.encode(
-#                     payload,
-#                     app.config["JWT_SECRET_KEY"],
-#                     algorithm="HS256"
-#                 )
-
-#                 # Redirect to the dashboard route with the token as a query parameter
-#                 return redirect(url_for('dashboard', token=access_token))
-
-#             except Exception as e:
-#                 return jsonify({
-#                     "message": "Something went wrong",
-#                     "error": str(e),
-#                     "data": None
-#                 }), 500
-#         else:
-#             return jsonify({
-#                 "message": "Invalid email or password",
-#                 "data": None,
-#                 "error": "Unauthorized"
-#             }), 401
-
-#     except Exception as e:
-#         return jsonify({
-#             "message": "Something went wrong",
-#             "error": str(e),
-#             "data": None
-#         }), 500
-# it will take the section argument ex: /sectionusers?section="cse-e"
 @app.route("/sectionusers", methods=["GET"])
 @jwttoken_required
 @role_required(allowed_roles=["admin", "teacher"])
@@ -534,16 +293,11 @@ def changepassword(current_user):
             "data": None
         }), 400
 
-# Teacher creates a class and generates a QR code
-
-
 @app.route("/create_class", methods=["POST"])
-@jwttoken_required  # Ensure only teachers can create classes
+@jwttoken_required
 @role_required(allowed_roles=["teacher"])
 def create_class(current_user):
     try:
-        # Check for timing conflicts with existing classes
-        # Check for timing conflicts with existing classes
         class_details = request.json
         start_time = datetime.datetime.strptime(class_details["start_time"], '%Y-%m-%dT%H:%M')
         end_time = datetime.datetime.strptime(class_details["end_time"], '%Y-%m-%dT%H:%M')
@@ -576,69 +330,6 @@ def create_class(current_user):
             "error": str(e),
             "data": None
         }), 500
-
-# @app.route("/join_class", methods=["POST"])
-# @jwttoken_required
-# @role_required(allowed_roles=["admin","student","teacher"])
-# def join_class():
-#     try:
-#         # # Get the class details based on the class name
-#         # token = request.args.get("token")  # Get the token from the query parameters
-#         # user_email = extract_email_from_token(token, app.config["JWT_SECRET_KEY"])
-#         data = request.json
-#         class_name = data.get('class_name')
-#         print(class_name)
-#         class_details = Classes().get_class_by_name(class_name)
-#         print(class_details)
-#         if class_details:
-#             # Extract the user's email from the device (replace with your method)
-#             token = None
-#             if "Authorization" in request.headers:
-#                 token = request.headers["Authorization"].split(" ")[1]
-#             # Check if the user's email exists in the user collection
-#             user = User().find_user_by_email(user_email)
-#             decoded_token = jwt.decode(token, app.config["JWT_SECRET_KEY"], algorithms=["HS256"])
-#             print(decoded_token)
-#             user_email = decoded_token["email"]  # Replace with the student's email
-
-#             # print(user, "ejhrgbkrjgnerwkgnk")
-#             if user:
-#                 # Implement logic to mark the user as present in a new collection
-#                 print("panduuuuuuuuuuu")
-#                 # Assuming that class_details and user objects have different structures
-#                 class_id = class_details.get("_id")  # Access class_id based on its actual structure
-#                 user_id = user.get("_id")  # Access user_id based on its actual structure
-#                 print(class_id, user_id, " fjhsbgvjhb")
-#                 Classes().mark_user_as_present(class_id, user_id)
-
-#                 # Fetch the QR code image for display
-#                 qr_code_image_path = fetch_qr_code_image(class_details['qr_code_path'])
-#                 print(qr_code_image_path)
-
-#                 return jsonify({
-#                     "message": "Student joined the class",
-#                     "class_details": class_details,
-#                     "user_email": user_email,
-#                     "qr_code_image_path": qr_code_image_path
-#                 }), 200
-#             else:
-#                 return jsonify({
-#                     "message": "User with email not found",
-#                     "data": None,
-#                     "error": "Unauthorized"
-#                 }), 401
-#         else:
-#             return jsonify({
-#                 "message": "Class with the specified name not found",
-#                 "data": None,
-#                 "error": "Not Found"
-#             }), 404
-#     except Exception as e:
-#         return jsonify({
-#             "message": "Failed to join class",
-#             "error": str(e),
-#             "data": None
-#         }), 500
 @app.route("/join_class", methods=["POST"])
 @jwttoken_required
 @role_required(allowed_roles=["student"])
@@ -652,12 +343,12 @@ def join_class(current_user):
 
         class_details = Classes().get_class_by_name(class_name)
         user = User().find_user_by_email(user_email)
+        print(class_details, user, "ejhrgbkrjgnerwkgnk")
         if class_details:
             meet_link = class_details.get('meet_link')
             user_id = user.get("_id")  # Access user_id based on its actual structure
-            class_id = class_details.get("_id")  # Access class_id based on its actual structure
+            class_id = class_details.get("class_id")  # Access class_id based on its actual structure
             user_id = user.get("_id")
-            # print(class_id, user_id, " fjhsbgvjhb")
             Classes().mark_user_as_present(class_id, user_id)
             if meet_link:
                 return jsonify({
